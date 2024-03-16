c5a2b2eb6af7b4cd480e5fe9bdacbf4f0ff4815a
Konadu-Akwasi-Akuoko <konaduakwasiakuoko@gmail.com>
2024-03-16 13:53:22 +0000

    doc: starting with chapter 4

---
+++ notes/04-the-anatomy-of-a-class/README.md
@@ -0,0 +1,1 @@
+# The Anatomy of A Class
---
+++ src/main/java/com/example/chapter_04/Main.java
@@ -0,0 +1,9 @@
+package com.example.chapter_04;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+    
+}
